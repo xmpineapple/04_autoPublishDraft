76ba8c408c44ae1698fe73f1b406c09ea88aee92
“xmpineapple” <hhq19926@gmail.com>
2026-06-06 22:27:32 +0800

    deleted:    README.md    modified:   Scripts/activate    modified:   Scripts/activate.bat    modified:   Scripts/pip.exe    modified:   Scripts/pip3.12.exe    modified:   Scripts/pip3.exe    modified:   pyvenv.cfg

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\traePractice\04_AIWeChatauto_prac"
+set "VIRTUAL_ENV=D:\traePractice\04_autoPublishDraft"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(04_AIWeChatauto_prac) %PROMPT%
+set PROMPT=(04_autoPublishDraft) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
-set "VIRTUAL_ENV_PROMPT=(04_AIWeChatauto_prac) "
+set "VIRTUAL_ENV_PROMPT=(04_autoPublishDraft) "
 
 :END
 if defined _OLD_CODEPAGE (
